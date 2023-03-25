55262059199d900818739acf2ba97f159ac9db0e
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2023-03-24 23:20:15 -0300

    com retorno e com parametro

---
+++ POO/ambos/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class App{
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    double resTotal = 0;
+    System.out.println("escolha a operação:");
+    System.out.println("1 - soma\n2 - subtração\n3 - multiplicação\n4 - divisão");
+    int res = in.nextInt();
+    if(res > 4 || res < 1){
+      System.out.println("numero invalido");
+    }else{
+      Calc calc = new Calc();
+      System.out.println("ok, digite agora dois numeros para fazer a operação escolhida: ");
+      int n1 = in.nextInt();
+      System.out.println("ok, agora digite o segundo: ");
+      int n2 = in.nextInt();
+      if(res == 1){
+        resTotal = calc.somar(n1, n2);
+      }else if(res == 2){
+        resTotal = calc.subtrair(n1, n2);
+      }else if(res == 3){
+        resTotal = calc.multiplicar(n1, n2);
+      }else if(res == 4){
+        resTotal = calc.dividir(n1, n2);
+      }
+      System.out.println("o resultado da operação é: "+resTotal);
+    }
+    in.close();
+  }
+}
