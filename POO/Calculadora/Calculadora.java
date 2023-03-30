21b03f3bed2f13d820f109d7819ed77e3d0bd792
Westwitt <112172527+Westwitt@users.noreply.github.com>
2023-03-30 10:18:15 -0300

    Sobrecarga scripts

--- POO/Calculadora/Calculadora.java
+++ POO/Calculadora/Calculadora.java
@@ -7,13 +7,29 @@ public class Calculadora {
 	public void som() {
 		res = n1+n2;
 	}
-	public void sub() {
-		res = n1-n2;		
+	public double som(double n1, double n2) {
+		res = n1+n2;
+		return res;
+	}
+	public double sub() {
+		res = n1-n2;
+		return res;
+	}
+	public void sub(double n1, double n2) {
+		res = n1-n2;
 	}
 	public void div() {
-		res = n1/n2;	
+		res = n1/n2;
 	}
-	public void mut() {
-		res = n1*n2;	
-	}	
+	public double div(double n1, double n2) {
+		res = n1/n2;
+		return res;
+	}
+	public double mut() {
+		res = n1*n2;
+		return res;
+	}
+	public void mut(double n1, double n2) {
+		res = n1*n2;
+	}
 }
